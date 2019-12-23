720dc78d90519cc520b76dddbaece6739bb77547
Jaskaran Singh <jaskaran101@hotmail.ca>
2019-12-22 19:56:02 -0500

    Added Tutorial Link

--- Senti.py
+++ Senti.py
@@ -2,7 +2,7 @@
 Author: Jaskaran Singh 
 Start Date: DEC 17 2019 
 End Date: Dec __ 2019
-
+https://www.datacamp.com/community/tutorials/text-analytics-beginners-nltk
 Description: Take in Sentences from User Process it and output the sentiment
 
 """
@@ -17,6 +17,6 @@ tokens = nltk.word_tokenize(sentense)
 print(tokens)
 
 tagged = nltk.pos_tag(tokens)
-tagged[0:6]
+tagged[0:7]
 
 print(tagged)
